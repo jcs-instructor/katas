765d1506b5212ac7bf2e59fdca861d122f908b5e
Ethan Strominger <ethanstrominger2@gmail.com>
2022-05-04 11:58:23 -0400

    Introduced makeApp

--- HelloWorld/app.ts
+++ HelloWorld/app.ts
@@ -1,40 +1,46 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { MobTimer } from './mobTimer';
-const app = express()
-const mobTimer = new MobTimer();
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+
+const app = makeApp();
+export function makeApp() {
+  const app = express()
+  const mobTimer = new MobTimer();
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
 
 
-app.get('/', (req, res) => {
-  res.send(mobTimer.state);
-})
+  app.get('/', (req, res) => {
+    res.send(mobTimer.state);
+  })
 
-app.post('/', (req, res) => {
-  const data = req.body;
-  mobTimer.durationMinutes = Number(data.duration);
+  app.post('/', (req, res) => {
+    const data = req.body;
+    mobTimer.durationMinutes = Number(data.duration);
 
-  // mobTimer.durationMinutes = data.durationMinutes || mobTimer.durationMinutes;
-  res.send(mobTimer.state);
-})
+    // mobTimer.durationMinutes = data.durationMinutes || mobTimer.durationMinutes;
+    res.send(mobTimer.state);
+  })
 
-app.get('/start', (req, res) => {
-  mobTimer.start();
-  res.send(mobTimer.state);
-});
+  app.get('/start', (req, res) => {
+    mobTimer.start();
+    res.send(mobTimer.state);
+  });
 
-app.get('/pause', (req, res) => {
-  mobTimer.pause();
-  res.send(mobTimer.state);
-});
+  app.get('/pause', (req, res) => {
+    mobTimer.pause();
+    res.send(mobTimer.state);
+  });
 
-app.get('/resume', (req, res) => {
-  mobTimer.resume();
-  res.send(mobTimer.state);
-});
+  app.get('/resume', (req, res) => {
+    mobTimer.resume();
+    res.send(mobTimer.state);
+  });
+
+  return app;
+};
 
 
 export default app;
--- HelloWorld/mobTimerRoutes.test.ts
+++ HelloWorld/mobTimerRoutes.test.ts
@@ -1,7 +1,20 @@
 import request from "supertest";
-import app from "./app";
+import { makeApp } from './app'
+
 test("testing post duration works", async () => {
-    const agent = request(app);
+    const agent = request(makeApp());
     const resp = await agent.post('/').send({ duration: 32 });
     expect(resp.body.duration).toEqual(32);
+});
+
+test("testing each agent independent", async () => {
+    const agent = request(makeApp());
+    const resp = await agent.get('/start');
+    expect(resp.body.status).toEqual("RUNNING");
+});
+
+test("testing each agent independent", async () => {
+    const agent = request(makeApp());
+    const resp = await agent.get('/');
+    expect(resp.body.status).toEqual("READY");
 });
