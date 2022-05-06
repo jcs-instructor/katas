f5da1a003d76214a4acea710ac6856aee2b44f9d
Ethan Strominger <ethanstrominger2@gmail.com>
2022-05-06 13:59:46 -0400

    Added join test with faked implementation

--- HelloWorld/webSocket.test.ts
+++ HelloWorld/webSocket.test.ts
@@ -1,82 +1,112 @@
 import WS from 'jest-websocket-mock';
-import { MobTimer } from './mobTimer';
+const wssUrl = "wss://localhost:1234";
 
-// TODO: After all, do WS.clean()
-test("Test can create subscriber", async () => {
-    const mobTimer = new MobTimer();
-    const subscriberId = "123.09.12.14"
-    const mobName = "arrested-egg";
-    // called whenever client does an http call
-    const subscriber = mobTimer.createOrGetSubscriber(subscriberId, mobName);
-    const subscriberForSecondRequest = mobTimer.createOrGetSubscriber(subscriberId, mobName);
-    expect(subscriberForSecondRequest).toBeInstanceOf(WS);
-    expect(subscriberForSecondRequest).toEqual(subscriber);
-    expect(subscriber.messages).toEqual([]);
-});
-
-test("Create two subscribers", () => {
-    const mobTimer = new MobTimer();
-    const mobName = "arrested-egg";
-    const subscriberId1 = "123.09.12.14";
-    const subscriber1 = mobTimer.createOrGetSubscriber(subscriberId1, mobName);
-    const subscriberId2 = "999.99.99.99";
-    const subscriber2 = mobTimer.createOrGetSubscriber(subscriberId2, mobName);
-    expect(subscriber1).not.toStrictEqual(subscriber2)
-});
-
-test("Test can create two subscribers same id different mob", () => {
-    const mobTimer = new MobTimer();
-    const subscriberId = "123.09.12.14"
-    const mobName = "arrested-egg";
-    const mobNameOther = "boring";
-    const subscriber = mobTimer.createOrGetSubscriber(subscriberId, mobName);
-    expect(subscriber).toBeInstanceOf(WS);
-    expect(mobTimer.createOrGetSubscriber(subscriberId, mobName)).toStrictEqual(subscriber);
-    expect(mobTimer.createOrGetSubscriber(subscriberId, mobNameOther)).not.toStrictEqual(subscriber);
-});
-
-test("Test sending mobTimer.state", async () => {
-    const mobTimer = new MobTimer();
-    const subscriberId = "123.09.12.14"
-    const mobName = "arrested-egg";
-    const subscriber = mobTimer.createOrGetSubscriber(subscriberId, mobName);
-    // Code to be put on to client, here to verify sending
-    const messagesReceivedBySubscriber = { subscriber: [] };
-    subscriber.onmessage = (e) => {
-        messagesReceivedBySubscriber.subscriber.push(e.data);
+class MockServer {
+    private _server: WS;
+    constructor(wssUrl) {
+        this._server = new WS(wssUrl);
+    }
+    public get server() {
+        return this._server;
     };
-    // End
-    mobTimer.sendMessage(mobName, mobTimer.state);
-    expect(messagesReceivedBySubscriber.subscriber).toEqual(JSON.stringify(mobTimer.state));
+}
+
+class MobServer {
+    mob(mobName: string) {
+        const mob = this.sockets.filter(elem =>
+            elem.mobName === "arrested-egg"
+        );
+        console.log(mob);
+        return mob;
+    }
+
+    private _sockets = [];
+
+    static configure = (server: WS): MobServer => {
+        const mobServer = new MobServer();
+        server.on("connection", () => mobServer.sockets.push({ mobName: "arrested-egg" }));
+        return mobServer;
+    }
+
+    public get sockets() { return this._sockets };
+}
+
+test("New mob server has zero websockets connected", async () => {
+    const mockServer = new MockServer(wssUrl);
+    const configuredServer = MobServer.configure(mockServer.server);
+    expect(configuredServer.sockets.length).toEqual(0);
+    mockServer.server.close();
 });
 
-test("Test sending messages with subscribers in different mobs", async () => {
-    const mobTimer = new MobTimer();
-    const mobName = 'arrested-egg';
-    const arrestedSubscriberId1 = "arrested.1";
-    const arrestedSubscriber1 = mobTimer.createOrGetSubscriber(arrestedSubscriberId1, mobName);
-    const arrestedSubscriberId2 = "arrested.2";
-    const arrestedSubscriber2 = mobTimer.createOrGetSubscriber(arrestedSubscriberId2, mobName);
-    const arrestedSubscriberId3 = "other.1";
-    const otherMobName = "other";
-    const otherSubscriber3 = mobTimer.createOrGetSubscriber(arrestedSubscriberId3, otherMobName);
-    // Code to be put on to client, here to verify sending
-    const messagesReceivedBySubscriber = { subscriber1: [], subscriber2: [], subscriber3: [] };
-    arrestedSubscriber1.onmessage = (e) => {
-        messagesReceivedBySubscriber.subscriber1.push(e.data);
-    };
-    arrestedSubscriber2.onmessage = (e) => {
-        messagesReceivedBySubscriber.subscriber2.push(e.data);
-    };
-    otherSubscriber3.onmessage = (e) => {
-        messagesReceivedBySubscriber.subscriber3.push(e.data);
-    };
-    // End
-    mobTimer.sendMessage(mobName, mobTimer.state);
-    expect(messagesReceivedBySubscriber.subscriber1[0]).toEqual([JSON.stringify(mobTimer.state)]);
-    expect(messagesReceivedBySubscriber.subscriber2).toEqual([JSON.stringify(mobTimer.state)]);
-    expect(messagesReceivedBySubscriber.subscriber3.length).toEqual(0);
+
+test("New mob server has one websocket connected", async () => {
+    const mockServer = new MockServer(wssUrl);
+    const configuredServer = MobServer.configure(mockServer.server);
+    new WebSocket(wssUrl);
+    await mockServer.server.connected;
+    mockServer.server.close();
+    expect(configuredServer.sockets.length).toEqual(1);
 });
+
+test("Joining a mob", async () => {
+    const mockServer = new MockServer(wssUrl);
+    const configuredServer = MobServer.configure(mockServer.server);
+    const client1 = new WebSocket(wssUrl);
+    await mockServer.server.connected;
+    client1.send(JSON.stringify({ action: "join", mob: "arrested-egg" }));
+    console.log("debug", configuredServer.sockets);
+    mockServer.server.close();
+    expect(configuredServer.mob("arrested-egg").length).toEqual(1);
+});
+
+
+// test("New mob server has one websocket when websocket opened", async () => {
+//     const server = new MockServer(wssUrl);
+//     const serverInspector = MobServer.configure(server);
+//     const client1 = new WebSocket(wssUrl);
+//     await server.connected;
+//     expect(serverInspector.sockets.length).toEqual(1);
+// });
+
+
+// TODO: Wrong test?  No server initialized so will not be able to send message?
+// test("Can send mob state to a client", async () => {
+
+//     const mobTimer = new MobTimer();
+//     const client1Socket = new WebSocket("ws://localhost:1234");
+//     const messages = new Map();
+//     client1Socket.onmessage = (e) => {
+//         messages.set(client1Socket, e.data);
+//     }
+//     mobTimer.join(client1Socket);
+//     mobTimer.sendState();
+// });
+
+// test("Can send mob state to multiple clients", async () => {
+//     const server = new WS("ws://localhost:1234");
+//     const mobTimer = new MobTimer();
+//     const sockets = [];
+//     const messages = new Map();
+//     for (let x = 1; x <= 3; x++) {
+//         const socket = new WebSocket("ws://localhost:1234");
+//         sockets.push(socket);
+//         socket.onmessage = (e) => {
+//             messages.set(socket, e.data);
+//         }
+//         mobTimer.join(socket);
+//         await server.connected();
+//     }
+
+//     mobTimer.sendState();
+
+//     sockets.forEach(socket => {
+//         const message = messages.get(socket);
+//         const messageParsed = JSON.parse(message);
+//         expect(JSON.parse(messageParsed)).toEqual(mobTimer.state);
+//     });
+// });
+
+// TODO: test("With multiple mobs, send mob state only to clients that joined that mob");
 
 // copied from https://www.npmjs.com/package/jest-websocket-mock
 test("example test that mock server sends messages to connected clients", async () => {
